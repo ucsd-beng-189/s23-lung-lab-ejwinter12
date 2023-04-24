664e4feab8e36b8c3d574dc74f7a265e22bfd163
ejwinter12 <ejwinter@ucsd.edu>
2023-04-23 22:42:25 -0700

    Fixed easy bug for task 3

--- lung.m
+++ lung.m
@@ -3,26 +3,26 @@ clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
 i=0;% chose index variable
-betavals = 0:0.1:1; %chose beta values
+betavals = 0:0.01:1; %chose beta values
+Mcoeff = 1;
 for beta = betavals %over each beta value
     i=i+1; %increase index each iteration
 setup_lung 
 cvsolve 
 outchecklung
 
-
 PIhold(i) = PI; %hold value of PI at that beta
-PAbar(i) = PAbar; %hold value of PI at that beta
-Pabar(i) = Pabar; %hold value of PI at that beta
-Pv(i) = Pv; %hold value of PI at that beta
+PAbarhold(i) = PAbar; %hold value of PI at that beta
+Pabarhold(i) = Pabar; %hold value of PI at that beta
+Pvhold(i) = Pv; %hold value of PI at that beta
 end
 
 figure(4) 
 hold on; 
 plot(betavals, PIhold,'-.','LineWidth',5) %plot variables vs beta
-plot(betavals, PAbar,'-.','LineWidth',5)
-plot(betavals, Pabar,'-.','LineWidth',5)
-plot(betavals, Pv,'-.','LineWidth',5)
+plot(betavals, PAbarhold,'-.','LineWidth',5)
+plot(betavals, Pabarhold,'-.','LineWidth',5)
+plot(betavals, Pvhold,'-.','LineWidth',5)
 xlabel('Beta')
 ylabel('values')
 title('Variations with Beta')
--- setup_lung.m
+++ setup_lung.m
@@ -25,7 +25,7 @@ cstar=cref;
 %in blood expressed in moles/liter)
 %
 %rate of oxygen consumption (moles/minute):
-M=0.25*cref*5.6;
+M=Mcoeff*0.25*cref*5.6;
 %
 %oxygen partial pressure 
 %at which hemoglobin is half-saturated:
@@ -63,8 +63,8 @@ av=(a1+a2)/2;
 VA=VAbar*(a1*beta+av*(1-beta));
 Q = Qbar*(a2*beta+av*(1-beta));
 r=VA./Q;
-figure(1)
-plot(Q,VA,'.')
+%figure(1)
+%plot(Q,VA,'.')
 
 %find actual values of 
 %VAtotal, Qtotal, VAbar, and Qbar:
